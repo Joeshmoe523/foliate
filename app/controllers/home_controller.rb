09c79be08130f1b52967bf62b08ea455910d614e
Joseph Yao <josephyao@Josephs-MacBook-Pro.local>
2025-10-13 09:32:53 -0700

    Added simple calendar gem

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,5 +5,6 @@ class HomeController < ApplicationController
   def dash
     @reflection = Reflection.new
     @growth_plans = current_user.growth_plans
+    @reflections = current_user.reflections.group_by { |r| r.created_at.to_date }
   end
 end
